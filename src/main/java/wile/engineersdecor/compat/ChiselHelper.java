0587346b2375d98b4af3eff824e3a0928bf56ddf
strubium <113206902+strubium@users.noreply.github.com>
2024-03-21 15:29:51 -0500

    Create ChiselHelper.java

---
+++ src/main/java/wile/engineersdecor/compat/ChiselHelper.java
@@ -0,0 +1,45 @@
+/*
+ * Based on ChiselHelper.java from ImmersiveEngineering by BluSunrize
+ */
+
+package wile.engineersdecor.compat;
+
+import net.minecraft.block.Block;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraftforge.fml.common.event.FMLInterModComms;
+
+public class ChiselHelper extends IECompatModule
+{
+	@Override
+	public void preInit()
+	{
+	}
+
+	@Override
+	public void init()
+	{
+		addVariation("treated_wood", IEContent.blockTreatedWood, 0);
+		addVariation("treated_wood", IEContent.blockTreatedWood, 1);
+		addVariation("treated_wood", IEContent.blockTreatedWood, 2);
+		addVariation("steel_scaffold", IEContent.blockMetalDecoration1, 1);
+		addVariation("steel_scaffold", IEContent.blockMetalDecoration1, 2);
+		addVariation("steel_scaffold", IEContent.blockMetalDecoration1, 3);
+		addVariation("aluminum_scaffold", IEContent.blockMetalDecoration1, 5);
+		addVariation("aluminum_scaffold", IEContent.blockMetalDecoration1, 6);
+		addVariation("aluminum_scaffold", IEContent.blockMetalDecoration1, 7);
+	}
+
+	@Override
+	public void postInit()
+	{
+	}
+
+	private void addVariation(String group, Block block, int meta)
+	{
+		NBTTagCompound tag = new NBTTagCompound();
+		tag.setString("group", group);
+		tag.setString("block", block.getRegistryName().toString());
+		tag.setInteger("meta", meta);
+		FMLInterModComms.sendMessage("chisel", "add_variation", tag);
+	}
+}
